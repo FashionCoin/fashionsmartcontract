1bfc6078951ea306e74fa367fc1c46e528b1eba3
root <rnOOBaxbC1>
2019-03-06 13:04:35 +0200

    start

---
+++ service.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+pkill -9 java
+
+cd /var/git/fashionsmartcontract
+nohup sh /var/git/fashionsmartcontract/start.sh > /var/log/fashion/smart.txt 2>&1 &
+
+tail -f /var/log/fashion/smart.txt
