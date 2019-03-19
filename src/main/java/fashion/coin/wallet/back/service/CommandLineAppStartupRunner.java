b7fe146c016e30d1d11a842e380e9bc44d8e1101
root <rnOOBaxbC1>
2019-03-19 03:44:13 +0200

    Remove key in start

--- src/main/java/fashion/coin/wallet/back/service/CommandLineAppStartupRunner.java
+++ src/main/java/fashion/coin/wallet/back/service/CommandLineAppStartupRunner.java
@@ -14,15 +14,8 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         try {
-
-            for (int i = 0; i < args.length; i++) {
-                System.out.println(i+" "+args[i]);
-            }
-
-
             String key = args[0];
             String vi = args[1];
-            System.out.println("aesEncriptor: "+aesEncriptor);
             aesEncriptor.setKey(key,vi);
         }catch (Exception e){
             System.out.println("You need to specify the key and initialization vector in the command line parameters");
