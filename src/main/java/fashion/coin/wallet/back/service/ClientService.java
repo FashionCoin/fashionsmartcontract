04b945ab582d552b1f6e2b25a96d19e8c761a305
root <rnOOBaxbC1>
2019-04-26 13:37:31 +0300

    test

--- src/main/java/fashion/coin/wallet/back/service/ClientService.java
+++ src/main/java/fashion/coin/wallet/back/service/ClientService.java
@@ -81,21 +81,13 @@ public class ClientService {
 
     private boolean checkValidCryptoname(String cryptoname) {
 
-        System.out.println(EmojiParser.parseToAliases(cryptoname, EmojiParser.FitzpatrickAction.REMOVE));
-
-        System.out.println("cryptoname: " + cryptoname);
-        System.out.println("cryptoname.length(): " + cryptoname.length());
         if (cryptoname.length() < 1) return false;
 
         String textWithoutEmoji = EmojiParser.removeAllEmojis(cryptoname);
         List<String> textOnlyEmoji = EmojiParser.extractEmojis(cryptoname);
 
-        System.out.println("textWithoutEmoji: " + textWithoutEmoji);
-        System.out.println("textOnlyEmoji: " + gson.toJson(textOnlyEmoji));
-        System.out.println("textWithoutEmoji.length()+textOnlyEmoji.size(): " + textWithoutEmoji.length() + textOnlyEmoji.size());
-
         if (textWithoutEmoji.length() + textOnlyEmoji.size() > 25) return false;
-        System.out.println("textWithoutEmoji.length() textOnlyEmoji.size():" + textWithoutEmoji.length() + " " + textOnlyEmoji.size());
+
         // Reserv:
         if (textWithoutEmoji.length() == 0 && textOnlyEmoji.size() == 1) return false;
 
@@ -103,8 +95,6 @@ public class ClientService {
         int charArrayLength = charArray.length;
         for (int i = 0; i < charArrayLength; i++) {
             char symbol = charArray[i];
-            System.out.println(symbol);
-            System.out.println("Code: " + (int) symbol);
             if (!Character.isJavaIdentifierPart(symbol) &&
                     !(Character.isLetter(symbol) && Character.isLowerCase(symbol)) &&
                     symbol != '-') return false;
