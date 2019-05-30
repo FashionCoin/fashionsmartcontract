df4240676e3327a3ae4a1967e3d9e888b8abba46
root <rnOOBaxbC1>
2019-05-30 12:21:11 +0300

    Skip create cryptoname

--- src/main/java/fashion/coin/wallet/back/telegram/FashionBot.java
+++ src/main/java/fashion/coin/wallet/back/telegram/FashionBot.java
@@ -39,7 +39,7 @@ public class FashionBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+//        System.out.println(update);
         if (update.hasCallbackQuery()) {
             System.out.println(update.getCallbackQuery().getData());
             if (update.getCallbackQuery().getData().equals(GOTOSECONDSCREEN)) {
