eae45c2c5316236d86c4f2599943231a27674618
root <rnOOBaxbC1>
2019-07-05 21:54:31 +0300

    Google Ref Install

--- src/main/java/fashion/coin/wallet/back/apple/controller/RefinstallController.java
+++ src/main/java/fashion/coin/wallet/back/apple/controller/RefinstallController.java
@@ -19,6 +19,22 @@ public class RefinstallController {
     @Autowired
     AppleRefService appleRefService;
 
+    @GetMapping("/api/v1/google/refinstall")
+    void redirectToGoogle(HttpServletRequest request, HttpServletResponse httpServletResponse,
+                         @RequestParam String api_key) {
+        String ipAddress = request.getHeader("X-FORWARDED-FOR");
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        String userAgent = request.getHeader("User-Agent");
+
+        appleRefService.registerDevice(api_key, ipAddress, userAgent);
+
+        httpServletResponse.setHeader("Location", "https://play.google.com/store/apps/details?id=wallet.fashion.coin");
+        httpServletResponse.setStatus(302);
+
+    }
     @GetMapping("/api/v1/apple/refinstall")
     void redirectToApple(HttpServletRequest request, HttpServletResponse httpServletResponse,
                          @RequestParam String api_key) {
@@ -31,7 +47,7 @@ public class RefinstallController {
 
         appleRefService.registerDevice(api_key, ipAddress, userAgent);
 
-        httpServletResponse.setHeader("Location", "https://www.apple.com/");
+        httpServletResponse.setHeader("Location", "https://www.apple.com/ios/app-store/");
         httpServletResponse.setStatus(302);
 
     }
