284a58bad858b3e38110a20b2859142e228048f1
root <rnOOBaxbC1>
2019-07-13 07:21:35 +0300

    gzip

--- service.sh
+++ service.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+dt=$(date '+%d%m%Y%H%M');
+
+cd /var/log/fashion/
+mv smart.txt smart"$dt".txt
+gzip  smart"$dt".txt
 
 cd /var/git/fashionsmartcontract
 nohup sh /var/git/fashionsmartcontract/start.sh > /var/log/fashion/smart.txt 2>&1 &
