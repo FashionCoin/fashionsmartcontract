4b97d524801f390f3b6b2ac0864b2f3feebc68ac
root <rnOOBaxbC1>
2019-08-08 14:15:12 +0300

    Remove Instagram

--- src/main/java/fashion/coin/wallet/back/telegram/FashionBot.java
+++ src/main/java/fashion/coin/wallet/back/telegram/FashionBot.java
@@ -2,7 +2,7 @@ package fashion.coin.wallet.back.telegram;
 
 
 import fashion.coin.wallet.back.telegram.screen.*;
-import fashion.coin.wallet.back.telegram.service.TelegramCheckService;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -15,24 +15,9 @@ public class FashionBot extends TelegramLongPollingBot {
 
     private String botToken;
 
-    public static final String GOTOSECONDSCREEN = "goToSecondScreen";
-    public static final String GOTOFIRSTROUTER = "goToFirstRouter";
-    public static final String GOTOSECONDROUTER = "goToSecondRouter";
-    public static final String GOTOTHIRDROUTER = "goToThirdRouter";
-    public static final String GOTOFOURTHROUTER = "goToFourthRouter";
-    public static final String GOTOFIFTHROUTER = "goToFifthRouter";
-    public static final String GOTOSIXROUTER = "goToSixRouter";
-    public static final String GOTOFSHNSCREEN = "goToFashionCoinScreen";
-    public static final String GOTOCREATENAMESCREEN = "goToCreateNameScreen";
-    public static final String GOTOREFERALSCREEN = "goToReferalScreen";
-    public static final String TMFSHNDONE = "telegramFashionDone";
-    public static final String TMEFSHNNEXT = "telegramFashionNext";
-    public static final String TMANNADONE = "telegramAnnaDone";
-    public static final String TMANNANEXT = "telegramAnnaNext";
-    public static final String INSTANNANEXT = "instagramAnnaNext";
     public static final String MYBALANCE = "myBalance";
     public static final String OLDBALANCE = "oldBalance";
-    public static final String CHECKEXISTSNAME = "checkExistsName";
+
 
 
     public FashionBot() {
@@ -48,53 +33,7 @@ public class FashionBot extends TelegramLongPollingBot {
         MyBalanceScreen.getInstance().execute(this, update);
     }
 
-//    @Override
-//    public void onUpdateReceived(Update update) {
-//        if (update.hasCallbackQuery()) {
-//            if (update.getCallbackQuery().getData().equals(GOTOFSHNSCREEN)) {
-//                FashionScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOFIRSTROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 0);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOSECONDROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 1);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOTHIRDROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 2);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOFOURTHROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 3);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOFIFTHROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 4);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOSIXROUTER)) {
-//                TelegramCheckService.getInstance().firstRouter(this, update, 5);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOCREATENAMESCREEN)) {
-//                TelegramCheckService.getInstance().checkExistsName(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(GOTOREFERALSCREEN)) {
-//                ReferalScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(MYBALANCE)) {
-//                MyBalanceScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(TMEFSHNNEXT)) {
-//                TAnnaScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(TMANNANEXT)) {
-//                InstAnnaScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(INSTANNANEXT)) {
-//                CryptoNameScreen.getInstance().execute(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(TMFSHNDONE)) {
-//                TelegramCheckService.getInstance().checkFashion(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(TMANNADONE)) {
-//                TelegramCheckService.getInstance().checkTAnna(this, update);
-//            } else if (update.getCallbackQuery().getData().equals(CHECKEXISTSNAME)) {
-//                TelegramCheckService.getInstance().checkExistsName(this, update);
-//            }
-//
-//        }
-//        if (update.hasMessage() && update.getMessage().hasText()) {
-//            if (update.getMessage().getText().contains("/start")) {
-//                FirstScreen.getInstance().execute(this, update);
-//            } else {
-//                TelegramCheckService.getInstance().checkText(this, update);
-//            }
-//
-//        }
-//    }
+
 
     @Override
     public String getBotUsername() {
--- src/main/java/fashion/coin/wallet/back/telegram/service/TelegramCheckService.java
+++ src/main/java/fashion/coin/wallet/back/telegram/service/TelegramCheckService.java
@@ -1,13 +1,11 @@
 package fashion.coin.wallet.back.telegram.service;
 
 
-import fashion.coin.wallet.back.dto.CryptonameTelegramDTO;
-import fashion.coin.wallet.back.dto.ResultDTO;
 import fashion.coin.wallet.back.entity.Client;
 import fashion.coin.wallet.back.service.ClientService;
 import fashion.coin.wallet.back.telegram.ContextProvider;
 import fashion.coin.wallet.back.telegram.FashionBot;
-import fashion.coin.wallet.back.telegram.screen.*;
+
 import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.ChatMember;
@@ -15,11 +13,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+
 import java.util.List;
 
+
 import static fashion.coin.wallet.back.telegram.FashionBot.MYBALANCE;
-import static fashion.coin.wallet.back.telegram.FashionBot.OLDBALANCE;
+
 
 
 public class TelegramCheckService {
@@ -27,7 +26,7 @@ public class TelegramCheckService {
     private static TelegramCheckService service = null;
     private static TelegramDataService dataService = null;
     private static ClientService clientService = null;
-    private static InstagramService instagramService = null;
+
     private static FashionBot bot = null;
 
     private TelegramCheckService() {
@@ -38,24 +37,11 @@ public class TelegramCheckService {
             service = new TelegramCheckService();
             dataService = ContextProvider.getBean(TelegramDataService.class);
             clientService = ContextProvider.getBean(ClientService.class);
-            instagramService = ContextProvider.getBean(InstagramService.class);
         }
         return service;
     }
 
 
-    public void checkFashion(FashionBot fashionBot, Update update) {
-        Integer userId = update.getCallbackQuery().getFrom().getId();
-        String isdone = dataService.getValue(userId.toString(), TELEGRAMFSHNDONE);
-        if (!"done".equals(isdone)) {
-            boolean isSubscribe = checkSubscribeTelegramFashon(fashionBot, update);
-            if (isSubscribe) {
-                increaceBalance(userId.toString(), "5000");
-                dataService.setValue(userId.toString(), TELEGRAMFSHNDONE, "done");
-            }
-        }
-        firstRouter(fashionBot, update, 2);
-    }
 
     private boolean checkSubscribeTelegramFashon(FashionBot fashionBot, Update update) {
         try {
@@ -77,18 +63,6 @@ public class TelegramCheckService {
 
     }
 
-    public void checkTAnna(FashionBot fashionBot, Update update) {
-        Integer userId = update.getCallbackQuery().getFrom().getId();
-        String isdone = dataService.getValue(userId.toString(), TANNADONE);
-        if (!"done".equals(isdone)) {
-            boolean isSubscribe = checkSubscribeTelegramAnna(fashionBot, update);
-            if (isSubscribe) {
-                increaceBalance(userId.toString(), "5000");
-                dataService.setValue(userId.toString(), TANNADONE, "done");
-            }
-        }
-        firstRouter(fashionBot, update, 3);
-    }
 
     private boolean checkSubscribeTelegramAnna(FashionBot fashionBot, Update update) {
         try {
@@ -111,39 +85,7 @@ public class TelegramCheckService {
 
     }
 
-    public void checkInstAnna(FashionBot fashionBot, Update update) {
-        Integer userId = update.getMessage().getFrom().getId();
-        String isdone = dataService.getValue(userId.toString(), INSTANNANAME);
-        if (!"done".equals(isdone)) {
-            String userAccount = update.getMessage().getText();
-            boolean isSubscribe = checkSubscribeInstagram(userAccount, "annakfashion");
-            if (isSubscribe) {
-                increaceBalance(userId.toString(), "5000");
-                dataService.setValue(userId.toString(), INSTANNANAME, userAccount);
-                dataService.setValue(userId.toString(), CURRENTSTEP, "");
-            } else {
-                dataService.setValue(userId.toString(), CURRENTSTEP, "");
-            }
-        }
-        TelegramCheckService.getInstance().firstRouter(fashionBot, update, 5);
-    }
 
-    public void checkInstFashion(FashionBot fashionBot, Update update) {
-        Integer userId = update.getMessage().getFrom().getId();
-        String isdone = dataService.getValue(userId.toString(), INSTFASHIONNAME);
-        if (!"done".equals(isdone)) {
-            String userAccount = update.getMessage().getText();
-            boolean isSubscribe = checkSubscribeInstagram(userAccount, "fashioncoin");
-            if (isSubscribe) {
-                increaceBalance(userId.toString(), "5000");
-                dataService.setValue(userId.toString(), INSTFASHIONNAME, userAccount);
-                dataService.setValue(userId.toString(), CURRENTSTEP, "");
-            } else {
-                dataService.setValue(userId.toString(), CURRENTSTEP, "");
-            }
-        }
-        TelegramCheckService.getInstance().firstRouter(fashionBot, update, 4);
-    }
 
     private void increaceBalance(String userId, String bonus) {
         String myBalance = dataService.getValue(userId, MYBALANCE);
@@ -154,83 +96,21 @@ public class TelegramCheckService {
     }
 
 
-    private boolean checkSubscribeInstagram(String userAccount, String followAccaunt) {
-        return instagramService.checkFollowing(userAccount, followAccaunt);
-    }
 
     public final String EXPERIENCED = "experienced";
     private final String TELEGRAMFSHNDONE = "telegramFashionDone";
     private final String TANNADONE = "telegramAnnaDone";
-    private final String INSTFASHIONNAME = "InstagramFashionDone";
-    private final String INSTANNANAME = "InstagramAnnaDone";
+
     public static final String CRYPTONAME = "CryptoName";
     private final String REFER = "Refer";
     public static final String CURRENTSTEP = "currentStep";
-    public static final String WAITINSTANNA = "waitInstagramAnna";
-    public static final String WAITINSTFASHION = "waitInstagramFashion";
+
     public static final String WAITNAME = "waitCryptonameName";
     public static final String BULKSENDED = "bulkSended";
 
-    public void checkText(FashionBot fashionBot, Update update) {
-//        System.out.println(update.getMessage().getText());
-        Integer userId = update.getMessage().getFrom().getId();
-        String currentStep = dataService.getValue(userId.toString(), CURRENTSTEP);
-        if (WAITINSTANNA.equals(currentStep)) {
-            checkInstAnna(fashionBot, update);
-        } else if (WAITINSTFASHION.equals(currentStep)) {
-            checkInstFashion(fashionBot, update);
-        } else if (WAITNAME.equals(currentStep)) {
-            checkCryptoname(fashionBot, update);
-        }
-    }
 
-    private void checkCryptoname(FashionBot fashionBot, Update update) {
-        Integer userId = update.getMessage().getFrom().getId();
-        String cryptoname = dataService.getValue(userId.toString(), CRYPTONAME);
-        if (cryptoname != null) {
-            ReferalScreen.getInstance().execute(fashionBot, update);
-            return;
-        }
-        cryptoname = update.getMessage().getText();
 
-        ResultDTO result = clientService.registerCryptoname(new CryptonameTelegramDTO(cryptoname, userId));
-        if (result.isResult()) {
-            dataService.setValue(userId.toString(), CRYPTONAME, cryptoname);
-            dataService.setValue(userId.toString(), WAITNAME, "");
-            dataService.setValue(userId.toString(), CURRENTSTEP, "");
-            increaceBalance(userId.toString(), "10000");
-            referalBonus(userId.toString());
-            CryptoDoneScreen.getInstance().execute(fashionBot, update);
-            return;
-        } else if (result.getError() == 100) {
-            CryptoExistsError.getInstance().execute(fashionBot, update);
-            return;
-        } else {
-            CryptoError.getInstance().execute(fashionBot, update);
-            return;
-        }
-    }
 
-//    public void goToFollowing(FashionBot fashionBot, Update update) {
-//        Integer userId = update.getCallbackQuery().getFrom().getId();
-//        String isdone = dataService.getValue(userId.toString(), TELEGRAMFSHNDONE);
-//        if (!"done".equals(isdone)) {
-//            FashionScreen.getInstance().execute(fashionBot, update);
-//            return;
-//        }
-//        isdone = dataService.getValue(userId.toString(), TANNADONE);
-//        if (!"done".equals(isdone)) {
-//
-//            TAnnaScreen.getInstance().execute(fashionBot, update);
-//            return;
-//        }
-//        isdone = dataService.getValue(userId.toString(), INSTANAME);
-//        if (!"done".equals(isdone)) {
-//            InstAnnaScreen.getInstance().execute(fashionBot, update);
-//            return;
-//        }
-//        SecondScreen.getInstance().execute(fashionBot, update);
-//    }
 
     public void checkReferal(Update update) {
 
@@ -255,60 +135,7 @@ public class TelegramCheckService {
         }
     }
 
-    public void checkExistsName(FashionBot fashionBot, Update update) {
 
-        Integer userId = update.getCallbackQuery().getFrom().getId();
-        String cryptoname = dataService.getValue(userId.toString(), CRYPTONAME);
-        if (cryptoname == null || cryptoname.length() == 0) {
-            CreateNameScreen.getInstance().execute(fashionBot, update);
-            return;
-        } else {
-            SkipCreationScreen.getInstance(cryptoname).execute(fashionBot, update);
-            return;
-        }
-
-    }
-
-
-    public void firstRouter(FashionBot fashionBot, Update update, int step) {
-        Integer userId = update.hasMessage() ?
-                update.getMessage().getFrom().getId() :
-                update.getCallbackQuery().getFrom().getId();
-        String experienced = dataService.getValue(userId.toString(), EXPERIENCED);
-        if (step > 0 || (experienced != null && experienced.length() > 0)) {
-            String telegramFashionDone = dataService.getValue(userId.toString(), TELEGRAMFSHNDONE);
-            if (step > 1 || (telegramFashionDone != null && telegramFashionDone.length() > 0)) {
-                String telegramAnnaDone = dataService.getValue(userId.toString(), TANNADONE);
-                if (step > 2 || (telegramAnnaDone != null && telegramAnnaDone.length() > 0)) {
-                    String InstagramFashion = dataService.getValue(userId.toString(), INSTFASHIONNAME);
-                    if (step > 3 || (InstagramFashion != null && InstagramFashion.length() > 0)) {
-                        String InstagramAnnaDone = dataService.getValue(userId.toString(), INSTANNANAME);
-                        if (step > 4 || (InstagramAnnaDone != null && InstagramAnnaDone.length() > 0)) {
-                            String CryptoName = dataService.getValue(userId.toString(), CRYPTONAME);
-                            if (step > 5 || (CryptoName != null && CryptoName.length() > 0)) {
-                                LastScreen.getInstance().execute(fashionBot, update);
-                            } else {
-                                CryptoNameScreen.getInstance().execute(fashionBot, update);
-                            }
-                        } else {
-                            InstAnnaScreen.getInstance().execute(fashionBot, update);
-                        }
-                    } else {
-                        InstFashionScreen.getInstance().execute(fashionBot, update);
-                    }
-                } else {
-                    TAnnaScreen.getInstance().execute(fashionBot, update);
-                }
-            } else {
-                FashionScreen.getInstance().execute(fashionBot, update);
-            }
-        } else {
-            dataService.setValue(userId.toString(), EXPERIENCED, "experienced");
-            SecondScreen.getInstance().execute(fashionBot, update);
-        }
-
-
-    }
 
     public String getBalance(String userId) {
         BigDecimal telegramBalance = clientService.getTelegramBalance(userId);
