2d6132c312e53fe7f610de5f4c6878c05b3433c6
root <rnOOBaxbC1>
2021-02-10 20:35:50 +0200

    Upload NFT Pictures

--- src/main/java/fashion/coin/wallet/back/nft/controller/FileStorageController.java
+++ src/main/java/fashion/coin/wallet/back/nft/controller/FileStorageController.java
@@ -2,6 +2,8 @@ package fashion.coin.wallet.back.nft.controller;
 
 import fashion.coin.wallet.back.dto.ResultDTO;
 import fashion.coin.wallet.back.service.FileUploadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,15 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileStorageController {
 
+    Logger logger = LoggerFactory.getLogger(FileStorageController.class);
+
     @Autowired
     FileUploadService fileUploadService;
 
     @PostMapping("/api/v1/file/nft/upload")
     @ResponseBody
     ResultDTO uploadFile(@RequestParam MultipartFile multipartFile, @RequestParam String apikey, @RequestParam String login) {
-
+        logger.info(multipartFile.getOriginalFilename());
         return fileUploadService.uploadNftPicture(multipartFile, login, apikey);
     }
 
