34f5c443c680daa6f3e76de248ae01877cd383d8
root <rnOOBaxbC1>
2021-03-03 11:31:21 +0200

    Error Buy Log

--- src/main/java/fashion/coin/wallet/back/nft/controller/BuyNftController.java
+++ src/main/java/fashion/coin/wallet/back/nft/controller/BuyNftController.java
@@ -1,8 +1,11 @@
 package fashion.coin.wallet.back.nft.controller;
 
+import com.google.gson.Gson;
 import fashion.coin.wallet.back.dto.ResultDTO;
 import fashion.coin.wallet.back.nft.dto.BuyNftDTO;
 import fashion.coin.wallet.back.nft.service.NftService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +15,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BuyNftController {
 
+    Logger logger = LoggerFactory.getLogger(BuyNftDTO.class);
+
     @Autowired
     NftService nftService;
 
+    @Autowired
+    Gson gson;
 
     @PostMapping("/api/v1/nft/buy")
     @ResponseBody
     ResultDTO buyNft(@RequestBody BuyNftDTO buyNftDTO) {
+        logger.info(String.valueOf(buyNftDTO));
         return nftService.buy(buyNftDTO);
     }
 
