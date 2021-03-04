7c7916e251d62f338361fb949e0e9c98ab07b2f9
root <rnOOBaxbC1>
2021-03-04 16:32:12 +0200

    Update Friends

--- src/main/java/fashion/coin/wallet/back/nft/service/ProofService.java
+++ src/main/java/fashion/coin/wallet/back/nft/service/ProofService.java
@@ -16,7 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.text.normalizer.CharTrie;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
