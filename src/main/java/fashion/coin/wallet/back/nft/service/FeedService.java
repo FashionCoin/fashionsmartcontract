59315aea7f6475b533ec98b92f4f38af4caddb2e
root <rnOOBaxbC1>
2021-03-04 17:13:23 +0200

    Feed NFT

--- src/main/java/fashion/coin/wallet/back/nft/service/FeedService.java
+++ src/main/java/fashion/coin/wallet/back/nft/service/FeedService.java
@@ -3,7 +3,9 @@ package fashion.coin.wallet.back.nft.service;
 import fashion.coin.wallet.back.dto.ResultDTO;
 import fashion.coin.wallet.back.entity.Client;
 import fashion.coin.wallet.back.nft.dto.FeedNftRequestDTO;
+import fashion.coin.wallet.back.nft.entity.FriendProof;
 import fashion.coin.wallet.back.nft.entity.Nft;
+import fashion.coin.wallet.back.nft.repository.FriendProofRepository;
 import fashion.coin.wallet.back.nft.repository.NftRepository;
 import fashion.coin.wallet.back.service.ClientService;
 import org.slf4j.Logger;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static fashion.coin.wallet.back.constants.ErrorDictionary.*;
@@ -29,10 +32,8 @@ public class FeedService {
 
     ClientService clientService;
     NftRepository nftRepository;
+    FriendProofRepository friendProofRepository;
 
-//    private List<Nft> mainFeed = new ArrayList<>();
-//    Long lastUpdateFeed = 0L;
-//    Nft lastActualNft = null;
 
     public ResultDTO getFeed(FeedNftRequestDTO request) {
         logger.info("Cryptoname: {} \t ApiKey: {}", request.getCryptoname(), request.getApikey());
@@ -42,81 +43,59 @@ public class FeedService {
             return error124;
         }
 
-        if (request.getFeedType().equals(MAIN_FEED)) {
-            try {
-                int fromIndex = (request.getPage() - 1) * request.getPerPage();
-                int toIndex = fromIndex + request.getPerPage();
 
-                List<Nft> mainFeed = nftRepository.findLastTenThousand();
-                /*
-                if (toIndex > mainFeed.size()) {
-                    getFeedTail(toIndex - mainFeed.size());
+        int fromIndex = (request.getPage() - 1) * request.getPerPage();
+        int toIndex = fromIndex + request.getPerPage();
 
-                    if (mainFeed.size() == 0) {
-                        return new ResultDTO(true, mainFeed, 0);
-                    }
-
-                    toIndex = mainFeed.size();
-                }
-                */
-
-                List<Nft> subList = mainFeed.subList(fromIndex, toIndex);
-//                cleanFeed(toIndex);
-                return new ResultDTO(true, subList, 0);
-            } catch (Exception e) {
-                logger.error(e.getMessage());
-                return error126;
-            }
-        } else if (request.getFeedType().equals(PROOF_FEED)) {
-            logger.info("Proof feed does not create");
-            return new ResultDTO(true, new ArrayList<>(), 0);
-        } else if (request.getFeedType().equals(PROOFS_FEED)) {
-            logger.info("Proofs feed does not create");
-            return new ResultDTO(true, new ArrayList<>(), 0);
-        } else {
-            return error125;
-        }
-
-    }
-
-    /*
-    private void cleanFeed(int toIndex) {
         try {
-            if (mainFeed.size() >= toIndex) {
-                Nft last = mainFeed.get(toIndex - 1);
-                if (lastActualNft == null || lastActualNft.getTimestamp() > last.getTimestamp()) {
-                    lastActualNft = last;
+            List<Nft> feed = new ArrayList<>();
+            if (request.getFeedType().equals(MAIN_FEED)) {
+
+                feed = nftRepository.findLastTenThousand();
+
+            } else if (request.getFeedType().equals(PROOF_FEED)) {
+                List<FriendProof> friendList = friendProofRepository.findByProofSenderId(client.getId());
+
+                for (FriendProof fp : friendList) {
+                    List<Nft> nftList = nftRepository.findByOwnerId(fp.getProofReceiverId());
+                    if (nftList != null && nftList.size() > 0) {
+                        feed.addAll(nftList);
+                    }
                 }
+                feed.sort((o1, o2) -> o2.getTimestamp().compareTo(o1.getTimestamp()));
+
+            } else if (request.getFeedType().equals(PROOFS_FEED)) {
+
+                List<FriendProof> friendList = friendProofRepository.findByProofReceiverId(client.getId());
+
+                for (FriendProof fp : friendList) {
+                    List<Nft> nftList = nftRepository.findByOwnerId(fp.getProofSenderId());
+                    if (nftList != null && nftList.size() > 0) {
+                        feed.addAll(nftList);
+                    }
+                }
+                feed.sort((o1, o2) -> o2.getTimestamp().compareTo(o1.getTimestamp()));
+            } else {
+                return error125;
             }
 
-            if (lastUpdateFeed + 100000L < System.currentTimeMillis()
-                    && lastActualNft != null) {
-                List<Nft> newList = mainFeed.subList(0, mainFeed.indexOf(lastActualNft) + 1);
-                mainFeed = newList;
-                lastActualNft = null;
-                lastUpdateFeed = System.currentTimeMillis();
+
+            if (fromIndex >= feed.size()) {
+                return new ResultDTO(true, new ArrayList<>(), 0);
             }
+
+            if (toIndex >= feed.size()) {
+                toIndex = feed.size();
+            }
+
+            List<Nft> subList = feed.subList(fromIndex, toIndex);
+            return new ResultDTO(true, subList, 0);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
+            return new ResultDTO(true, new ArrayList<>(), 0);
         }
     }
 
-    private void getFeedTail(int elements) {
-        Long lastNftTime = System.currentTimeMillis();
-        if (mainFeed.size() > 1) {
-            lastNftTime = mainFeed.get(mainFeed.size() - 1).getTimestamp();
-        }
-
-        logger.info("Last Time: {}",lastNftTime);
-        logger.info("Elements: {}",elements);
-
-        List<Nft> oldNfts = nftRepository.findByLocalDateTimeBeforeAndOrderByLocalDateTimeDescLimitedTo(lastNftTime, elements);
-        logger.info("Old Nfts: {}",oldNfts);
-        if (oldNfts != null && oldNfts.size() > 0) {
-            mainFeed.addAll(oldNfts);
-        }
-    }
-*/
     @Autowired
     public void setClientService(ClientService clientService) {
         this.clientService = clientService;
@@ -126,10 +105,9 @@ public class FeedService {
     public void setNftRepository(NftRepository nftRepository) {
         this.nftRepository = nftRepository;
     }
-/*
-    public void addNewNft(Nft nft) {
-        mainFeed.add(0, nft);
-    }
 
- */
+    @Autowired
+    public void setFriendProofRepository(FriendProofRepository friendProofRepository) {
+        this.friendProofRepository = friendProofRepository;
+    }
 }
