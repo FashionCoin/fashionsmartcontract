4ce6f9e7fdb8db2dfdaed480f7f001479bc9f1a9
root <rnOOBaxbC1>
2021-09-11 10:40:45 +0300

    Block NFT In Sale

--- src/main/java/fashion/coin/wallet/back/service/InitService.java
+++ src/main/java/fashion/coin/wallet/back/service/InitService.java
@@ -70,6 +70,7 @@ public class InitService {
         this.fileUploadService = fileUploadService;
     }
 
+    @Autowired
     public void setNftService(NftService nftService) {
         this.nftService = nftService;
     }
